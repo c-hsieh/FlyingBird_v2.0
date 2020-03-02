7efa632ae79c9a6f0a031808beabd59cd644f39c
謝長霖 <singularity@Aaron-MBP.local>
2020-03-03 03:12:41 +0800

    commit 5000

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -6,7 +6,7 @@ export default () => {
     <div>
       <h4 className="mt-1 justify-content-center mx-auto">
         <span style={{ fontSize: "0.8em", color: "#777777" }}>
-          因為此網站查詢資料是同步與學校資料庫要資料，如果校方資料庫使用人數爆滿，導致無法查詢，請耐心等候...稍後再試
+          因為此網站查詢資料是同步與學校資料庫要資料，如果校方資料庫使用人數爆滿，導致無法查詢時，請耐心等候...稍後再試
         </span>
       </h4>
       <img
