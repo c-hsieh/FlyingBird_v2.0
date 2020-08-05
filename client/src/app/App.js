2c88157b8d1362031cfe4c0cfb616eb5b896ba1c
Aaron <aaronhsieh1997@gmail.com>
2020-08-06 03:25:30 +0800

    add ga

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Navbar from '../components/layout/Navbar';
